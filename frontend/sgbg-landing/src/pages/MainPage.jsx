dc4aa9f0ffc6ebd54b890850d203388b440ba5c9
김재훈 <kgarry@naver.com>
2024-11-05 16:51:45 +0900

    feat: 랜딩 화면 다운로드 영역 틀 추가

--- frontend/sgbg-landing/src/pages/MainPage.jsx
+++ frontend/sgbg-landing/src/pages/MainPage.jsx
@@ -20,8 +20,8 @@ const s = {
     height: 700px;
 
     background-image: linear-gradient(rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5)), url(${SingBungSurfing});
-    background-position: right 0;
-    background-size: 700px 700px;
+    background-position: right 0 bottom 0;
+    background-size: 640px 640px;
     background-repeat: no-repeat;
   `,
   TitleText: styled.h1`
@@ -54,6 +54,41 @@ const s = {
 
     font-size: 28px;
   `,
+  DescriptionSection: styled.section`
+    height: 1000px;
+
+    background-color: #cccccc;
+  `,
+  DownloadSection: styled.section`
+    width: 100%;
+    min-height: 1500px;
+  `,
+  DownloadArea: styled.div`
+    margin: 0 5%;
+
+    display: flex;
+    width: 90%;
+    min-height: 700px;
+
+    background-color: white;
+  `,
+  DownloadTextArea: styled.div`
+    box-sizing: border-box;
+
+    width: 55%;
+    height: 500px;
+  `,
+  DownloadText: styled.span`
+    font-size: 96px;
+  `,
+  DownloadImgArea: styled.div`
+    box-sizing: border-box;
+
+    width: 45%;
+    height: 500px;
+
+    border-radius: 20px;
+  `,
 }
 
 const MainPage = () => {
@@ -78,6 +113,27 @@ const MainPage = () => {
           </s.DownloadButton>
         </s.DownloadButtonArea>
       </s.TitleSection>
+      <s.DescriptionSection></s.DescriptionSection>
+      <s.DownloadSection>
+        <s.DownloadArea>
+          <s.DownloadTextArea>
+            <s.DownloadText>싱글벙글 신나는 이미지 저장을 위해 싱글벙글 데스크톱 앱을 다운로드하세요</s.DownloadText>
+            <s.DownloadButton>
+              <WindowsLogo /> Windows 앱 다운로드
+            </s.DownloadButton>
+          </s.DownloadTextArea>
+          <s.DownloadImgArea></s.DownloadImgArea>
+        </s.DownloadArea>
+        <s.DownloadArea>
+          <s.DownloadImgArea></s.DownloadImgArea>
+          <s.DownloadTextArea>
+            <s.DownloadText>간편한 이미지 저장을 위해 크롬 확장 프로그램을 다운로드하세요</s.DownloadText>
+            <s.DownloadButton>
+              <ChromeLogo /> 확장 프로그램 다운로드
+            </s.DownloadButton>
+          </s.DownloadTextArea>
+        </s.DownloadArea>
+      </s.DownloadSection>
     </s.Container>
   )
 }
