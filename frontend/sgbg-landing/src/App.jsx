5d920d9aac12dacd7e596df02866ecd635e6274d
김재훈 <kgarry@naver.com>
2024-11-05 15:31:50 +0900

    feat: 랜딩 메인페이지 제목 섹션 구성

--- frontend/sgbg-landing/src/App.jsx
+++ frontend/sgbg-landing/src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
 import MainPage from './pages/MainPage'
 import GlobalStyle from './GlobalStyles'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <>
       <GlobalStyle />
+      <Navbar />
       <Routes>
         <Route path="/" element={<MainPage />} />
       </Routes>
---
+++ frontend/sgbg-landing/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+const s = {
+  Container: styled.div`
+    height: 60px;
+  `,
+}
+
+const Navbar = () => {
+  return (
+    <>
+      <s.Container>dd</s.Container>
+    </>
+  )
+}
+
+export default Navbar
