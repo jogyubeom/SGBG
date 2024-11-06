235e48265b262e23a1ac0bc3644e9470017c66df
조규범 <standardjo@naver.com>
2024-11-06 14:05:02 +0900

    1106 landinggggg

--- frontend/sgbg-landing/vite.config.js
+++ frontend/sgbg-landing/vite.config.js
@@ -5,5 +5,4 @@ import svgr from "vite-plugin-svgr";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  base: "/landing/", // 정적 리소스 경로를 /landing/으로 설정
 });
